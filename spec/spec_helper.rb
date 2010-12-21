ddf78dd32e44d4cf3004cddf8a87a2b9c6239c3b
Michael Barton <mail@michaelbarton.me.uk>
2010-12-21 21:15:54 +0000

    Prune unsused methods from spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -36,31 +36,4 @@ RSpec.configure do |config|
     settings
   end
 
-  def generate_sequences(count)
-    (1..count).to_a.map do |n|
-      { :name => "sequence#{n}",
-        :nucleotides => %w|A T G C A T G C|.sort_by{rand}.to_s }
-    end
-  end
-
-  def generate_scaffold(*entries)
-    entries.flatten.inject(Array.new) do |array,entry|
-      if entry.definition =~ /sequence/
-        array << {'sequence' => {'source' => entry.definition}}
-      else
-        array << {'unresolved' => {'length' => entry.sequence.length}}
-      end
-    end
-  end
-
-  def scaffold_validate(scaffold_file,sequence_file,*flags)
-    cmd = "./bin/scaffold-validate #{flags} #{scaffold_file} #{sequence_file}"
-    out = StringIO.new(`#{cmd}`)
-    if $? == 0
-      out.string
-    else
-      raise RuntimeError.new("Error executing scaffold-validate\n#{out.string}")
-    end
-  end
-
 end
