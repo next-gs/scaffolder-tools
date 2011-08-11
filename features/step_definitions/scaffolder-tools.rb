e6bc8894e613809dc46745dbd3aa0b022be87ecf
Michael Barton <mail@michaelbarton.me.uk>
2011-08-11 16:16:31 -0400

    Use backticks version of aruba run command step

--- features/step_definitions/scaffolder-tools.rb
+++ features/step_definitions/scaffolder-tools.rb
@@ -1,4 +1,4 @@
 When /^I call "([^"]*)" with arguments "([^"]*)"$/ do |command,args|
   bin = File.join(File.dirname(__FILE__),'..','..','bin',command)
-  When "I run \"#{bin} #{args}\""
+  When "I run `#{bin} #{args}`"
 end
